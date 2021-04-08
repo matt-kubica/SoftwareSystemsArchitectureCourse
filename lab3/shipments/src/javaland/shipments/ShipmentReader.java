fe463d6be4ca35611f974775f7a884624733c900
qbic22562 <mateusz.kubica99@gmail.com>
2021-04-08 20:25:20 +0200

    Added exception signature

--- lab3/shipments/src/javaland/shipments/ShipmentReader.java
+++ lab3/shipments/src/javaland/shipments/ShipmentReader.java
@@ -3,6 +3,6 @@ package javaland.shipments;
 import java.util.List;
 
 public interface ShipmentReader {
-    List <Shipment> readFile(String path);
+    List <Shipment> readFile(String path) throws Exception;
     String getFileType();
 }
